bbb434ae82ea1a267fe101c4a78b12f86d469733
aow1980 <wilkes.allen@gmail.com>
2016-06-29 20:13:01 -0400

    fix hyperopts for arm64 temp fix

--- core/hyperopts.mk
+++ core/hyperopts.mk
@@ -11,21 +11,6 @@ DISABLE_CORTEX_STRINGS := $(BLUETOOTH) *libc*
 my_cflags :=  $(filter-out -Wall -Werror -Werror=% -g,$(my_cflags))
 my_cppflags :=  $(filter-out -Wall -Werror -Werror=% -g,$(my_cppflags))
 
-# Force ARM Instruction Set
-ifndef LOCAL_IS_HOST_MODULE
-  ifneq (1,$(words $(filter $(DISABLE_ARM_MODE),$(LOCAL_MODULE))))
-    ifeq ($(LOCAL_ARM_MODE),)
-      LOCAL_ARM_MODE := arm
-      my_cflags += -marm
-      my_cflags :=  $(filter-out -mthumb,$(my_cflags))
-    endif
-  else
-    LOCAL_ARM_MODE := thumb
-    my_cflags += -mthumb
-    my_cflags :=  $(filter-out -marm,$(my_cflags))
-  endif
-endif
-
 # IPA Optimizations
 ifndef LOCAL_IS_HOST_MODULE
   ifeq (,$(filter true,$(my_clang)))
